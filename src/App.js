251ce2d0c61cbb498074a9634c1675d065632141
Michael Williams <manicdesigner@gmail.com>
2019-08-27 13:16:58 -0400

    useEffect has NasaApi function setting photo of day data import NasaPhoto component prop drill photo of day into NasaPhoto for display and altText

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { NasaApi } from './components/Nasa-api';
+import NasaPhoto from './components/Nasa-Photo';
+import './App.css';
+
+// data.explanation => info
+// data.data => 2019-08-27
+// data.hdurl => hi def image
+// data.title => image title (uses for alt on image)
+// data.url => url regular def image (probably will use this one)...
 
 function App() {
+  useEffect(() => {
+    NasaApi(setPhotoOfTheDay);
+  }, []);
+  const [photoOfTheDay, setPhotoOfTheDay] = useState();
+  console.log(photoOfTheDay);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <NasaPhoto photoOfTheDay={photoOfTheDay} />
     </div>
   );
 }
